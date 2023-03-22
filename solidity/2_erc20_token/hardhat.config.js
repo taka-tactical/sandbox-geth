a245d855a7f832e9a821a140c1975b75a908dc5d
Taka <taka717@gmail.com>
2023-03-22 22:50:01 +0900

    Update README_ja for ERC20 token sample

--- solidity/2_erc20_token/hardhat.config.js
+++ solidity/2_erc20_token/hardhat.config.js
@@ -1,14 +1,29 @@
 require("@nomicfoundation/hardhat-toolbox");
+require("@nomiclabs/hardhat-etherscan");
 require("dotenv").config();
+
+/** @type import('hardhat/config').NetworksUserConfig */
+var networks = {
+  private: {
+    url: "http://127.0.0.1:8545",
+    chainId: 12345,
+    accounts: [`0x${process.env.POANET_PRIVATE_KEY}`],
+  },
+};
+
+if (process.env.GOERLI_API_URL !== '' && process.env.GOERLI_PRIVATE_KEY !== '') {
+  networks['goerli'] = {
+    url: process.env.GOERLI_API_URL,
+    accounts: [`0x${process.env.GOERLI_PRIVATE_KEY}`],
+  };
+}
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
-  networks: {
-    private: {
-      url: "http://127.0.0.1:8545",
-      chainId: 12345,
-      accounts: [`${process.env.POA_PRIVATE_KEY}`],
-    },
-  },
   solidity: "0.8.18",
+  defaultNetwork: "hardhat",
+  networks: networks,
+  etherscan: {
+    apiKey: `${process.env.ETHERSCAN_API_KEY}`,
+  },
 };
